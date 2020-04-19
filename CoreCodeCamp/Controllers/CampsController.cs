95f1d2014d3d979abbc96aa70bc75223c2d55455
Imane El Boutachfaiti <imane.elboutachfaiti@gmail.com>
2020-04-19 07:25:07 +0200

    Returning models instead of entities + Return Related Data

--- CoreCodeCamp/Controllers/CampsController.cs
+++ CoreCodeCamp/Controllers/CampsController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using CoreCodeCamp.Data;
+using CoreCodeCamp.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,23 +15,41 @@ namespace CoreCodeCamp.Controllers
     public class CampsController : ControllerBase
     {
         private readonly ICampRepository _repository;
-        public CampsController(ICampRepository repository)
+        private readonly IMapper _mapper;
+
+        public CampsController(ICampRepository repository,IMapper mapper)
         {
             _repository = repository;
+            _mapper = mapper;
         }
+
         [HttpGet]
-        public async Task<IActionResult> Get()
+        public async Task<ActionResult<CampModel[]>> Get(bool includeTalks = false)
         {
             try
             {
-                var results = await (_repository.GetAllCampsAsync());
-                return Ok(results);
+                var results = await _repository.GetAllCampsAsync(includeTalks);
+                return _mapper.Map<CampModel[]>(results);
             }
             catch(Exception )
             {
                 return this.StatusCode(StatusCodes.Status500InternalServerError,"Database failure");
             }
          
+        }
+        [HttpGet("{monikor}")]
+        public async Task<ActionResult <CampModel>> Get( string monikor)
+        {
+            try
+            {
+                var result = await _repository.GetCampAsync(monikor);
+                if (result == null) return NotFound();
+                return _mapper.Map<CampModel>(result);
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database failure");
+            }
         }
     }
 }
--- CoreCodeCamp/Data/Entities/Location.cs
+++ CoreCodeCamp/Data/Entities/Location.cs
@@ -1,4 +1,4 @@
-﻿
+﻿ 
 namespace CoreCodeCamp.Data
 {
     public class Location
